94e950bd2df85fd9ce1fa5881d24eb984d208893
SpiderWacho <iamgaas@gmail.com>
2022-11-21 14:07:12 -0300

    Change in markup for styling

--- src/components/finalData.js
+++ src/components/finalData.js
@@ -1,83 +1,96 @@
-import { useState } from "react";
+import { React, Component } from "react";
 
 
 import './finalData.css';
 
 
-const FinalData = (props) => {
+class FinalData extends Component {
+    constructor(props) {
+        super(props);
 
-
-    const [data, setData] = useState({             
+    this.state = { 
         contact1: 'site.webpage.com',
         contact2: 'Twitter',
         contact3: 'Instagram',
         description: '"Write a short description about yourself"',
-    })
+    }
 
 
-   const textAreaAdjust = (element) => {
-    element.style.height = "5px";
-    element.style.height = (element.scrollHeight)+"px";
-  }
+   this.displayChange = this.displayChange.bind(this);
+}
 
-   const displayChange = (e) => {
-        setData({
-            [e.target.id] : e.target.value})
-   }
+   displayChange(e) {
+    this.setState({
+        [e.target.name] : e.target.value})
+}
+
+
+render(){
         return (
             <div className='final-container'>
                 <div className="bar">
                     <div className="contact-container">
-                    <input id="contact1" onChange={(e) => {displayChange(e)}} className="contact" value={data.contact1}></input>
-                    <input id="contact2" onChange={(e) => {displayChange(e)}} className="contact" value={data.contact2}></input>
-                    <input id="contact3" onChange={(e) => {displayChange(e)}} className="contact" value={data.contact3}></input>
+                    <input name="contact1" onChange={(e) => this.displayChange(e)} className="contact" value={this.state.contact1}></input>
+                    <input name="contact2" onChange={(e) => this.displayChange(e)} className="contact" value={this.state.contact2}></input>
+                    <input name="contact3" onChange={(e) => this.displayChange(e)} className="contact" value={this.state.contact3}></input>
                     </div>
-                <textarea maxLength="155" id="description" onKeyUp={(e) => {textAreaAdjust(e)}} onChange={(e) => {displayChange(e)}} className="description" value={data.description}></textarea>
+                <textarea name="description" maxLength="155" id="description" onChange={(e) => {this.displayChange(e)}} className="description" value={this.state.description}></textarea>
                 </div>
-                <img src={props.data.title.avatar} alt="Selected profile pic" className="profile-img"></img>
+                {(this.props.data.title.avatar !== '') && <img src={this.props.data.title.avatar} alt="Selected profile pic" className="profile-img"></img>}
                 <div className="name-container">
                     <div className="name-div">
-                        <p id="firstname">{props.data.title.firstName}</p>
-                        <p id="lastname">{props.data.title.lastName}</p>  
-                    </div>     
+                        <span className="name-row">
+                        <p id="firstname">{this.props.data.title.firstName}</p>
+                        </span>
+                        <span className="name-row">
+                        <p id="lastname">{this.props.data.title.lastName}</p>  
+                        </span>
+                    </div>      
+                    <div className="contact-info">           
+                            <p id="email">{this.props.data.title.email}</p>
+                            <p  id="tel">{this.props.data.title.telphone}</p>
+                            <p id="direction">{this.props.data.title.direction}</p>
+                    </div>
                 </div>
-                <div className="contact-info">           
-                        <p id="email">{props.data.title.email}</p>
-                        <p id="tel">{props.data.title.telphone}</p>
-                        <p id="direction">{props.data.title.direction}</p>
-                </div>
+
                 <div className="experience-container"> 
-                    <h2>Experiencie</h2>
-                    {props.data.experienceChilds.map( (experience, index) => {
-                        console.log(experience.experience)
+                <div className="separate-bar"></div>    
+                    <h2 className="finaldata-title">Experiencie</h2>
+                    {this.props.data.experienceChilds.map( (experience) => {
+
                         return (
-                            <div className="experience" data-key={index} key={index}>
-                            <p className="start-date">{experience.experience.start}</p>
-                            <p className="end-date">{experience.experience.end}</p>
-                            <p className="company">{experience.experience.company}</p>
-                            <p className="title">{experience.experience.title}</p>
-                            <p className="description">{experience.experience.description}</p>
-                            <button onClick={(e) => {props.removeExperiencie(e.target.parentNode.dataset.key, 'experienceChilds')}} className="delete" data-html2canvas-ignore="true">X</button>
+                            <div className="experience" key={experience.id}>
+                                <p className="start-date">{experience.start}</p>
+                                <p className="end-date">{experience.end}</p>
+                                <p className="company">{experience.company}</p>
+                                <p className="title">{experience.title}</p>
+                                <p className="description">{experience.description}</p>
+                                <button onClick={(e) => {this.props.removeElement(experience.id, 'experienceChilds')}} className="delete">X</button>
                             </div>
                         )
                     })}
+                
                 </div>
+                
                 <div className="education-container">
-                    <h2>Education</h2>
-                    {props.data.educationChilds.map( (education, index) => {
+                <div className="separate-bar"></div>    
+                    <h2 className="finaldata-title">Education</h2>
+                    {this.props.data.educationChilds.map( (education) => {
                         return (
-                            <div className="education" key={index}>
-                            <p className="start-date">{education.education.start}</p>
-                            <p className="end-date">{education.education.end}</p>
-                            <p className="title">{education.education.title}</p>
-                            <p className="institution">{education.education.institution}</p>
-                            <button onClick={(e) => {props.removeExperiencie(e.target.parentNode.dataset.key, 'educationChilds')}} className="delete" data-html2canvas-ignore="true">X</button>
+                            <div className="education"  key={education.id}>
+                                <p className="start-date">{education.start}</p>
+                                <p className="end-date">{education.end}</p>
+                                <p className="title">{education.title}</p>
+                                <p className="institution">{education.institution}</p>
+                                <button onClick={(e) => {this.props.removeElement(education.id, 'educationChilds')}} className="delete">X</button>
                             </div>
                         )
-                    })}
+                    })}   
                 </div>
+               
             </div>
         )
     }
+}
 
 export default FinalData;
