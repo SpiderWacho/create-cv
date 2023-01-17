0320ef29538a5802e8542a69d40c4c57f3c93cdd
SpiderWacho <iamgaas@gmail.com>
2023-01-17 17:19:14 -0300

    Add checks for empty fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+/* eslint-disable no-unused-vars */
 import { useState, useRef } from 'react';
 
 import './App.css';
 import { useReactToPrint } from 'react-to-print';
-import Title from './components/title';
-import Experiencie from './components/experience';
-import Education from './components/education';
-import FinalData from './components/finalData';
+import Title from './components/Title';
+import Experience from './components/Experience';
+import Education from './components/Education';
+import FinalData from './components/FinalData';
 import avatar from './images/avatarProfile.png';
 
 function App() {
@@ -13,39 +14,93 @@ function App() {
   const [education, setEducation] = useState([]);
   const [title, setTitle] = useState({
     avatar,
-    firstName: 'Name',
-    lastName: 'Last name',
-    telphone: 'Phone Number',
-    email: 'example@mail.com',
-    direction: 'Direction'
+    firstName: 'Nombre',
+    lastName: 'Apellido',
+    telphone: 'Numero de telefono',
+    email: 'ejemplo@mail.com',
+    direction: 'Direccion'
   });
 
+  const [titleError, setTitleError] = useState(false);
+  const [experienceError, setExperienceError] = useState(false);
+  const [educationError, setEducationError] = useState(false);
+
+  const checkForErrors = (input) => {
+    if (input === '') {
+      return true;
+    }
+    return false;
+  };
+
+  const deactivateAllErrors = () => {
+    setTitleError(false);
+    setExperienceError(false);
+    setEducationError(false);
+  };
+
   const handleInfo = (formData) => {
-    setTitle((prevstate) => ({ ...prevstate, ...formData }));
+    deactivateAllErrors();
+    // eslint-disable-next-line no-restricted-syntax
+    for (const [key, value] of Object.entries(formData)) {
+      if (key !== 'avatar') {
+        if (checkForErrors(value)) {
+          setTitleError(true);
+          return;
+        }
+        setEducationError(false);
+      }
+    }
+    if (!titleError) {
+      setTitle((prevstate) => ({ ...prevstate, ...formData }));
+    }
   };
 
   const addExperience = (child) => {
+    deactivateAllErrors();
     const id = experiences.length + 1;
-    const childToAppend = { ...child };
-    childToAppend.id = id;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const [key, value] of Object.entries(child)) {
+      if (checkForErrors(value)) {
+        setExperienceError(true);
+        return;
+      }
+      setExperienceError(false);
+    }
+    if (!experienceError) {
+      const childToAppend = { ...child };
+      childToAppend.id = id;
 
-    setExperiences((prevState) => [...prevState, childToAppend]);
-    const inputs = document.querySelectorAll('.input-field');
-    inputs.forEach((input) => {
-      input.value = '';
-    });
+      setExperiences((prevState) => [...prevState, childToAppend]);
+      const inputs = document.querySelectorAll('.input-field');
+      inputs.forEach((input) => {
+        // eslint-disable-next-line no-param-reassign
+        input.value = '';
+      });
+    }
   };
 
   const addEducation = (child) => {
+    deactivateAllErrors();
     const id = education.length + 1;
-    const childToAppend = { ...child };
-    childToAppend.id = id;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const [key, value] of Object.entries(child)) {
+      if (checkForErrors(value)) {
+        setEducationError(true);
+        return;
+      }
+      setEducationError(false);
+    }
+    if (!educationError) {
+      const childToAppend = { ...child };
+      childToAppend.id = id;
 
-    setEducation((prevState) => [...prevState, childToAppend]);
-    const inputs = document.querySelectorAll('.input-field');
-    inputs.forEach((input) => {
-      input.value = '';
-    });
+      setEducation((prevState) => [...prevState, childToAppend]);
+      const inputs = document.querySelectorAll('.input-field');
+      inputs.forEach((input) => {
+        // eslint-disable-next-line no-param-reassign
+        input.value = '';
+      });
+    }
   };
 
   const removeExperience = (index) => {
@@ -73,15 +128,19 @@ function App() {
         <h1>Cv Creator</h1>
         <span className="download-container">
           <button type="button" onClick={handlePrint} className="download">
-            Download CV
+            Descargar CV
           </button>
         </span>
       </div>
       <div className="content">
         <div className="modifiable-section">
-          <Title handleInfo={handleInfo} />
-          <Experiencie handleInfo={handleInfo} addExperiencie={addExperience} />
-          <Education handleInfo={handleInfo} addEducation={addEducation} />
+          <Title handleInfo={handleInfo} error={titleError} />
+          <Experience
+            handleInfo={handleInfo}
+            addExperience={addExperience}
+            error={experienceError}
+          />
+          <Education handleInfo={handleInfo} addEducation={addEducation} error={educationError} />
         </div>
         <div id="final-section">
           <FinalData
